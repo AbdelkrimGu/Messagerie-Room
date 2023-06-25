8fca694ef50edf79587b3e73d27b088958fef9df
Abdelkrim <abdelkrimguenanou@gmail.com>
2023-06-25 11:23:13 +0200

    check if user is present

--- index.js
+++ index.js
@@ -37,10 +37,13 @@ io.on("connection",(socket) => {
 
     socket.on("sendMessage" , ({senderId , receiverId , text}) => {
         const user = getUser(receiverId);
-        io.to(user.socketId).emit("getMessage" , {
-            senderId,
-            text,
-        });
+        if(user){
+            io.to(user.socketId).emit("getMessage" , {
+                senderId,
+                text,
+            });
+        }
+        
     });
 
     socket.on("disconnect" , ()=>{
